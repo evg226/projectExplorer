c0d09a20499d7c1633a7bc26ec5e1a0e7374aee2
evg226 <evg226@hotmail.com>
2021-11-05 14:48:55 +0500

    Создание модели сущностей и связей между сущностями:
    1. Проектируем модели  в draw.io на https://app.diagrams.net/
    2. Описываем модели в models.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
+const models = require("./models/models");
 
 const PORT = process.env.PORT || 5001;
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 const start = async () => {
     try {
         await sequelize.authenticate();
+        await sequelize.sync();
         app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
     } catch (error) {
         console.log("ERROR: " + error);
---
+++ backend/models/models.js
@@ -0,0 +1,104 @@
+const sequelize = require("../db");
+const { DataTypes } = require("sequelize");
+
+// Models description
+
+const User = sequelize.define("user", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    email: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING },
+    role: { type: DataTypes.STRING, defaultValue: "USER" }
+});
+
+const Basket = sequelize.define("basket", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const BasketProject = sequelize.define("basket_project", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+
+const Project = sequelize.define("project", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING,unique: true,allowNull:false },
+    description: { type: DataTypes.STRING },
+    start: { type: DataTypes.DATE },
+    finish: { type: DataTypes.DATE },
+    rating: { type: DataTypes.INTEGER }
+});
+
+const Type = sequelize.define("type", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING,unique: true,allowNull:false },
+});
+
+const Author = sequelize.define("author", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING,unique: true,allowNull:false },
+});
+
+const Rating = sequelize.define("rating", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    rate: { type: DataTypes.STRING, allowNull:false }
+});
+
+const Img = sequelize.define("img", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    path: { type: DataTypes.STRING, allowNull:false }
+});
+
+const Stack = sequelize.define("stack", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.STRING, allowNull:false }
+});
+
+const TypeAuthor = sequelize.define("type_author", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+// Models relations
+
+User.hasOne(Basket);
+Basket.belongsTo(User);
+
+User.hasMany(Rating);
+Rating.belongsTo(User);
+
+Basket.hasMany(BasketProject);
+BasketProject.belongsTo(Basket);
+
+Type.hasMany(Project);
+Project.belongsTo(Type);
+
+Author.hasMany(Project);
+Project.belongsTo(Author);
+
+Project.hasMany(Rating);
+Rating.belongsTo(Project);
+
+Project.hasOne(BasketProject);
+BasketProject.belongsTo(Project);
+
+Project.hasMany(Img);
+Img.belongsTo(Project);
+
+Project.hasMany(Stack);
+Stack.belongsTo(Project);
+
+Type.belongsToMany(Author, { through: TypeAuthor });
+Author.belongsToMany(Type, { through: TypeAuthor });
+
+module.exports = {
+    User,
+    Rating,
+    Basket,
+    BasketProject,
+    Project,
+    Type,
+    Author,
+    Img,
+    Stack
+}
