c1472e51061e2dc7363f7197b1df28c915e08f90
evg226 <evg226@hotmail.com>
2021-11-05 15:16:59 +0500

    Настройка cors

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,22 @@
-require("dotenv").config();
-const express = require("express");
-const sequelize = require("./db");
-const models = require("./models/models");
+require("dotenv").config(); //загрузка переменных окружения
+const express = require("express");//использование express.js
+const sequelize = require("./db");//загрузка описания подключения к db postresql через ORM sequelize
+const models = require("./models/models"); // загрузка описания модели и связей 
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5001;
 const app = express();
 
+// настройка cors
+app.use(cors());
+app.use(express.json());
+
+// описание инициализации сервера
 const start = async () => {
     try {
         await sequelize.authenticate();
         await sequelize.sync();
-        app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+        app.listen(PORT, () => console.log(`\n\nServer has started on port ${PORT}`));
     } catch (error) {
         console.log("ERROR: " + error);
     }
